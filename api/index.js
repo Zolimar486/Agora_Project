7281505470edb69e0cc9046e1225ec88c2cc1b67
Zolimar486 <ing.carrasquero.zolimar@gmail.com>
2024-04-01 15:48:41 -0300

    Updated

--- api/index.js
+++ api/index.js
@@ -7,27 +7,32 @@ dotenv.config()
 const serviceRouter = require('./routers/service')
 const postRouter= require('./routers/post')
 
-const app =express()
-const port=process.env.PORT || 5000
+
 
 //DataBase connections
-mongoose.set('strictQuery', false)
 
 mongoose.connect(process.env.MONGO_URL)
+
 .then(()=> console.log("MongoDb Connected"))
+
 .catch((err)=> console.log(err))
 
 
 //Middleware Functions
+const app =express()
 app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({extended: true, limit:'50mb'}))
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors())
-app.get('/info', (req,res)=>{
-  res.status(200).json({message:"Successfully Connected"})
-})
 app.use('/api/service', serviceRouter)
 app.use('/api/post', postRouter)
+app.get('/', (req,res)=>{
+  res.status(200).json({message:"Successfully Connected"})
+})
+
+
+
+const port=process.env.PORT || 5000
 
 
 app.listen(port, ()=> console.log(`The server is running on : http://localhost:${port}`))
